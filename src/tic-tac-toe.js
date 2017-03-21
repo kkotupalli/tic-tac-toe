4eefbe9df5f33af81e57d7c184821629490fd3f6
kkotupalli <kartheek_kotupalli@epam.com>
2017-03-21 18:00:07 +0530

    updated the unit tests

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,9 +1,9 @@
 class TicTacToe {
 
-   constructor() {
-
-       this.currentPlayerSymbol = 'x';
-       this.ticTacMatrix = TicTacToe.matrixCreation(3, 3, null);
+    constructor() {
+        this.currentPlayerSymbol = 'x';
+        this.matrix = TicTacToe.createMatrix(3, 3, null);
+        this.gridScore = TicTacToe.createMatrix(2 * 3 + 2, 1, 0);
     }
 
     getCurrentPlayerSymbol() {
@@ -15,49 +15,69 @@ class TicTacToe {
         if (currentVal != null) {
             return;
         }
-        this.ticTacMatrix[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+        this.matrix[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
+        this.addScore(rowIndex, columnIndex, this.getCurrentPlayerSymbol() === 'x' ? 1 : -1);
         this.changePlayer();
-
     }
 
     isFinished() {
-        
+        return this.getWinner() != null || this.isDraw();
     }
 
     getWinner() {
-        
-
+        for (let i = 0; i < this.gridScore.length; i++) {
+            let scoreItem = this.gridScore[i];
+            if (Math.abs(scoreItem) === 3) {
+                return scoreItem > 0 ? 'x' : 'o';
+            }
+        }
+        return null;
     }
 
     noMoreTurns() {
-        
+        for (let i = 0; i < this.matrix.length; i++) {
+            for (let j = 0; j < this.matrix[i].length; j++) {
+                if (this.matrix[i][j] == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     isDraw() {
-       
+        return this.getWinner() == null && this.noMoreTurns();
     }
 
     getFieldValue(rowIndex, colIndex) {
-        return this.ticTacMatrix[rowIndex][colIndex];
+        return this.matrix[rowIndex][colIndex];
     }
     changePlayer() {
         this.currentPlayerSymbol = this.getCurrentPlayerSymbol() === 'x' ? 'o' : 'x';
     }
-    addGridScore(row, col, point) {
-        
+    addScore(row, col, point) {
+        this.gridScore[row][0] += point;
+        this.gridScore[3 + col][0] += point;
+        if (row == col) {
+            this.gridScore[2 * 3][0] += point;
+        }
+        if (3 - 1 - col == row) {
+            this.gridScore[2 * 3 + 1][0] += point;
+        }
     }
 
-    static matrixCreation(rows, columns, initialValue) {
-        let rowArray = [];
-        for (let rowVal = 0; rowVal < rows; rowVal++) {
-            let columnArray = [];
-            for (let colVal = 0; colVal < columns; colVal++) {
-                columnArray[colVal] = initialValue;
+    static createMatrix(rows, cols, initialValue) {
+        let rowArr = [];
+        for (let i = 0; i < rows; i++) {
+            let colArr = [];
+            for (let j = 0; j < cols; j++) {
+                colArr[j] = initialValue;
             }
-            rowArray[rowVal] = columnArray;
+            rowArr[i] = colArr;
         }
-        return rowArray;
+        return rowArr;
     }
 }
 
 module.exports = TicTacToe;
+
